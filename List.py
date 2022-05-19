149354107f437b73f6be0589a58f612a837b4c98
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-05-19 19:47:59 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,14 @@
+# It is an data structure used to store an multiple elements in single variable ... 
+list1=[1,2,3,4,5,6]
+list2=["jelly","ani","pru"]
+print(list1)
+print(list2)
+print("\n 1-3 elemenyts " + str(list1[1:3]))
+print("\n last elemt "+ str(list1[-1]))
+print("\n gap of 2 "+str(list1[0::2]))
+print("\n Adding two list "+ str(list1.extend(list2)))
+print("\n Append :"+str(list1.append(7)))
+print("\n Pop : "+str(list1.pop()))
+
+
+
