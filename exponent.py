04bf0c31e04aeb830ea24aa0dab85f6052c46f05
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-05-19 19:47:13 +0530

    Add files via upload

---
+++ exponent.py
@@ -0,0 +1,11 @@
+
+#power function using for loop 
+def raise_to_power(base,power):
+    result=1
+    for index in range(power):
+        result=result*base
+    return result
+
+base=int(input("Enter an base : "))
+power=int (input("Enter an power : "))
+print("power is : ", raise_to_power(base,power))
