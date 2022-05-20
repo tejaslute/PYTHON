f4c4d7b2b9c426a18bc63633d4fa5cdea02daee6
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-05-20 11:41:06 +0530

    Update BSF.py

--- BSF.py
+++ BSF.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #Create an garph using distionary 
 graph = {
   '1' : ['2','3','4'],
@@ -25,32 +25,4 @@ def bsf(graph,visited,node):
 
 #calling function 
 print("BSF of graph is : ")
-=======
-#Create an garph using distionary 
-graph = {
-  '1' : ['2','3','4'],
-  '3' : ['1', '2','5'],
-  '2' : ['1','3'],
-  '4' : ['1'],
-  '5' : ['3']
-}
-
-visited=[]
-queue=[]
-
-def bsf(graph,visited,node):
-    visited.append(node)
-    queue.append(node)
-    while queue:
-        m=queue.pop(0)
-        print(m,end="->")
-        for neighbour in graph[m]:
-             if neighbour not in visited:
-                queue.append(neighbour)
-                visited.append(neighbour)
-
-
-#calling function 
-print("BSF of graph is : ")
->>>>>>> d6ee4b2618c1fb7ef0bcbd735f05c543dfec0e7c
 bsf(graph,visited,'1')
